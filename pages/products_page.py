335bb025c5e2b1c38a08bc552f41dbf3ee7ef4ec
Bradley <yapi.bradley@gmail.com>
2025-12-10 15:40:34 -0500

    fix: Applied JavaScript click to add_bikelight_to_cart method to fix E2E timeout

--- pages/products_page.py
+++ pages/products_page.py
@@ -39,16 +39,21 @@ class ProductsPage:
 
         print("Article 'Sac à Dos' ajouté au panier par force JavaScript.")
 
+
     def add_bikelight_to_cart(self):
-        """Ajoute la lampe de vélo au panier en assurant la synchronisation."""
+        """Ajoute le Bikelight au panier en forçant le clic via JavaScript."""
 
-        self.wait.until(EC.element_to_be_clickable(self.ADD_TO_CART_BIKELIGHT))
+        # 1. Attendre que le bouton ADD soit présent et cliquable
+        add_button = self.wait.until(EC.element_to_be_clickable(self.ADD_TO_CART_BIKELIGHT))
 
-        self.driver.find_element(*self.ADD_TO_CART_BIKELIGHT).click()
+        # 2. CORRECTION : Exécuter le clic via JavaScript
+        self.driver.execute_script("arguments[0].click();", add_button)
 
-        # Attente pour la confirmation de l'ajout
-        self.wait.until(EC.visibility_of_element_located(self.REMOVE_BIKELIGHT))
-        print("Article 'Lampe de Vélo' ajouté au panier et confirmation reçue.")
+        # 3. Synchronisation : Attendre que le badge du panier contienne un nombre > 1 (il est déjà là après le sac à dos)
+        # L'attente de la présence du bouton REMOVE_BIKELIGHT est la plus directe
+        self.wait.until(EC.presence_of_element_located(self.REMOVE_BIKELIGHT))
+
+        print("Article 'Bikelight' ajouté au panier par force JavaScript.")
 
     def get_cart_count(self):
         """Retourne le nombre d'articles dans le panier."""
